273d829015c42178c69113c27a15ba76410522bc
Melinda Hardin <melindawad@gmail.com>
2018-08-13 21:13:37 -0500

    none

--- client/src/components/FooterTitle/FooterTitle.js
+++ client/src/components/FooterTitle/FooterTitle.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const styling={
-    fontSize: 12
+    fontSize: 13
 }
 
 const FooterTitle = ()=>(
 <div style ={styling}>
-    <p>2018 T.A.G-FRESH!!! website created by wadesites</p>
+    <p>2018 T.A.G-FRESH website created by wadesites</p>
 </div>
 )
 export default FooterTitle;
