387ea3e7dca10a9e6f9a1ecbaa4ae1f1d1d89251
Melinda Hardin <melindawad@gmail.com>
2018-08-13 21:42:52 -0500

    contact form

--- client/src/components/Jumbotrons/Contact/Contact.js
+++ client/src/components/Jumbotrons/Contact/Contact.js
@@ -5,10 +5,25 @@ const Contact =props=>(
   <div className="jumbotron jumbotron-fluid" id="contact">
     <div className="container">
       <h3 className="display-4">Contact</h3>
-        
+        <form>
+          <div className="form-group">
+            <label for="exampleFormControlInput1">Name</label>
+            <input type="txt" className="form-control" id="exampleFormControlInput1"/>{props.name}
+          </div>
+
+          <div className="form-group">
+            <label for="exampleFormControlInput1">Email address</label>
+            <input type="email" className="form-control" id="exampleFormControlInput1"/>{props.email}
+          </div>
+
+          <div className="form-group">
+            <label for="exampleFormControlTextarea1">Message</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="6">{props.message}</textarea>
+          </div>
+          <button type="button" className="btn btn-primary">Submit</button>
+        </form>
 
     </div>
   </div>
-
 )
 export default Contact;
--- client/src/components/Jumbotrons/Landing/Landing.js
+++ client/src/components/Jumbotrons/Landing/Landing.js
@@ -2,19 +2,27 @@ import React from "react";
 import images from "../../../images/mia.JPG";
 
 const styling={
-  backgroundImage: "url(" + images + ")",
-  backgroundSize: "contain",    
-  backgroundPosition: "right",
-  backgroundRepeat: "no-repeat",
-  height: 650,
-  backgroundColor: "#BDC2C8",
-  padding: 10,
-  marginBottom: 280
+  image:{
+    backgroundImage: "url(" + images + ")",
+    backgroundSize: "contain",    
+    backgroundPosition: "right",
+    backgroundRepeat: "no-repeat",
+    height: 650,
+    backgroundColor: "#BDC2C8",
+    padding: 10,
+    marginBottom: 280
+  },
+
+  subtitle:{
+    padding: 80
+  }
+  
 }
 
 const Landing = ()=>(
-<div style = {styling}>
-    <p className="lead" id="tag">The Ambassador...
+<div style = {styling.image}>
+    <p className="lead" id="tag" style={styling.subtitle}>The Ambassador...
+    <br/>
     DJ Guaranteed-Fresh!!!</p>
 </div> 
 
