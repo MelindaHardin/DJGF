88ed7d7c47bb4a0cb9a0c6d59e23a991cf4ff539
Melinda Hardin <melindawad@gmail.com>
2018-08-17 22:55:21 -0500

    albums json

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -1,25 +1,47 @@
 import React from "react";
+import albums from "../../albums.json";
 
 const styling = {
   cards:{
     width: "25rem"
   }
 }
-const Cards = props =>(
-<div className="row justify-content-center">
-  <div className="col-4">
-      <div className="card" style= {styling.cards}>
-        <img className="card-img-top" src={props.img} alt={props.name}/>
-        <div className="card-body">
-          <h5 className="card-title">{props.name}</h5>
-        
-          <p className="card-text">{props.details}</p>
-          <button type="button" className="btn btn-primary" data-toggle="modal">
-              View Songs
-            </button>
+
+class Cards extends React.Component{
+
+  state = {
+    albums,
+    clicked:[]
+  }
+
+
+  handleClicked = id =>{
+
+
+  }
+
+  render (){
+
+    return (
+      <div className="row justify-content-center">
+        <div className="col-4">
+            <div className="card" style= {styling.cards}>
+              <img className="card-img-top" src={this.props.img} alt={this.props.name}/>
+              <div className="card-body">
+                <h5 className="card-title">{this.props.name}</h5>
+              
+                <p className="card-text">{this.props.details}</p>
+                <button type="button" className="btn btn-primary" data-toggle="modal">
+                    View Songs
+                  </button>
+              </div>
+            </div>
         </div>
       </div>
-  </div>
-</div>
-);
-export default Cards
+    )
+  }
+
+}  
+ 
+export default Cards;
+
--- client/src/components/Jumbotrons/Mixes/Mixes.js
+++ client/src/components/Jumbotrons/Mixes/Mixes.js
@@ -2,6 +2,7 @@ import React from "react";
 import Cards from "../../Cards/Cards"
 
 const Mixes = () => (
+
   <div>
     <h4 className="display-4">Mixes</h4>
     <Cards/>
